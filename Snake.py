0daac987b9d54aa8fda9ea60022f197f27cfb0ee
HausenSjtu <sjtuzhs@sjtu.edu.cn>
2016-06-22 17:15:10 +0800

    小心蛇跑到屏幕外面去。哈哈

--- Snake.py
+++ Snake.py
@@ -7,9 +7,9 @@ from Const import *
 
 
 SNAKE_WITH_HALH = 10
-INIT_V = 5
-INIT_DIRECT = 0
-INIT_POS = (10,50,50,50)    #pos定义方式，首末两点的坐标
+INIT_V = 1
+INIT_DIRECT = 3
+INIT_POS = [10,50,200,50]    #pos定义方式，首末两点的坐标
 SNAKE_COLOR=(0,255,100) #我喜欢荧光绿的蛇
 
 class Snake:
@@ -42,13 +42,13 @@ class Snake:
 
     def _SetDirection(self,d):
         lastPos = self.snakePoss[-1]
-        if (lastPos[1]==lastPos[3]) and (d==2||d==3):  #蛇的新方向和旧方向都在水平方向
+        if (lastPos[1]==lastPos[3]) and (d==2 or d==3):  #蛇的新方向和旧方向都在水平方向
             if self._ToLeft(-1):   
                 self.direction = 2
             else:
                 self.direction = 3
             self.dirChange = False
-        elif (lastPos[0]==lastPos[2]) and (d==0||d==1):    #都是竖直方向
+        elif (lastPos[0]==lastPos[2]) and (d==0 or d==1):    #都是竖直方向
             if self._ToUp(-1):   
                 self.direction = 0
             else:
@@ -69,14 +69,24 @@ class Snake:
     def _UpdateRects(self):
         self.myRects=[]
         for i,pos in enumerate(self.snakePoss):
+##            if self._ToLeft(i):
+##                self.myRects.append(Rect(pos[2],pos[3]-SNAKE_WITH_HALH,pos[0],pos[1]+SNAKE_WITH_HALH))
+##            elif self._ToRight(i):
+##                self.myRects.append(Rect(pos[0],pos[1]-SNAKE_WITH_HALH,pos[2],pos[3]+SNAKE_WITH_HALH))
+##            elif self._ToUp(i):
+##                self.myRects.append(Rect(pos[2]-SNAKE_WITH_HALH,pos[3],pos[0]+SNAKE_WITH_HALH,pos[1]))
+##            elif self._ToDown(i):
+##                self.myRects.append(Rect(pos[0]-SNAKE_WITH_HALH,pos[1],pos[2]+SNAKE_WITH_HALH,pos[3]))
+##            else:
+##                print "error in _UpdateRects"
             if self._ToLeft(i):
-                self.myRects.append((pos[2],pos[3]-SNAKE_WITH_HALH,pos[0],pos[1]+SNAKE_WITH_HALH))
+                self.myRects.append(Rect(pos[2],pos[3]-SNAKE_WITH_HALH,pos[0]-pos[2]+1,pos[1]+SNAKE_WITH_HALH-(pos[3]-SNAKE_WITH_HALH)+1))
             elif self._ToRight(i):
-                self.myRects.append((pos[0],pos[1]-SNAKE_WITH_HALH,pos[2],pos[3]+SNAKE_WITH_HALH))
+                self.myRects.append(Rect(pos[0],pos[1]-SNAKE_WITH_HALH,pos[2]-pos[0]+1,pos[3]+SNAKE_WITH_HALH-(pos[1]-SNAKE_WITH_HALH)+1))
             elif self._ToUp(i):
-                self.myRects.append((pos[2]-SNAKE_WITH_HALH,pos[3],pos[0]+SNAKE_WITH_HALH,pos[1]))
+                self.myRects.append(Rect(pos[2]-SNAKE_WITH_HALH,pos[3],pos[0]+SNAKE_WITH_HALH-(pos[2]-SNAKE_WITH_HALH)+1,pos[1]-pos[3]+1))
             elif self._ToDown(i):
-                self.myRects.append((pos[0]-SNAKE_WITH_HALH,pos[1],pos[2]+SNAKE_WITH_HALH,pos[3]))
+                self.myRects.append(Rect(pos[0]-SNAKE_WITH_HALH,pos[1],pos[2]+SNAKE_WITH_HALH-(pos[0]-SNAKE_WITH_HALH)+1,pos[3]-pos[1]+1))
             else:
                 print "error in _UpdateRects"
 
@@ -84,82 +94,149 @@ class Snake:
         self._UpdateRects()
         for rect in self.myRects:
             self.pygame.draw.rect(self.screen,SNAKE_COLOR,rect)
-            
+
+
+    def __DeleteTail(self,length):
+        if length<2*SNAKE_WITH_HALH+1:
+            self.snakePoss.pop(0)
+        else:
+            return
+        if self._ToLeft(0):
+            self.snakePoss[0][0]+=length
+        elif self._ToRight(0):
+            self.snakePoss[0][0]-=length
+        elif self._ToUp(0):
+            self.snakePoss[0][1]+=length
+        elif self._ToDown(0):
+            self.snakePoss[0][1]-=length
+        else:
+            print "error in _UpdateRects"
 
     def _Decrese(self,decDist):
         while 1:
-            for direct0,direct1,diff0,diff1 in [(0,2,1,3),(1,3,0,2)]:
-                if self.snakePoss[0][direct0]==self.snakePoss[0][direct1]:
-                    if self.snakePoss[0][diff0]>self.snakePoss[0][diff1]:
-                        if self.snakePoss[0][diff0]-self.snakePoss[0][diff1]>=moveDist:
-                            self.snakePoss[0][diff0]-=moveDist
-                            return
-                        elif self.snakePoss[0][diff0]-self.snakePoss[0][diff1]==moveDist:
-                            self.snakePoss.pop(0)
-                            return
-                        else:
-                            moveDist-=(self.snakePoss[0][diff0]-self.snakePoss[0][diff1])
-                            self.snakePoss.pop(0)
-                    else:
-                        if self.snakePoss[0][diff1]-self.snakePoss[0][diff0]>=moveDist:
-                            self.snakePoss[0][diff0]+=moveDist
-                            return
-                        elif self.snakePoss[0][diff1]-self.snakePoss[0][diff0]==moveDist:
-                            self.snakePoss.pop(0)
-                            return
-                        else:
-                            moveDist-=(self.snakePoss[0][diff1]-self.snakePoss[0][diff0])
-                            self.snakePoss.pop(0)
+            if self._ToLeft(0):
+                if self.snakePoss[0][0]-self.snakePoss[0][2]>decDist:
+                    self.snakePoss[0][0]-=decDist
+                    length = self.snakePoss[0][0]-self.snakePoss[0][2]
+                    self.__DeleteTail(length)
+                    return
+                elif self.snakePoss[0][0]-self.snakePoss[0][2]==decDist:
+                    self.snakePoss.pop(0)
+                    return
+                else:
+                    self.snakePoss.pop(0)
+                    decDist-=(self.snakePoss[0][0]-self.snakePoss[0][2])
+            elif self._ToRight(0):
+                if self.snakePoss[0][2]-self.snakePoss[0][0]>decDist:
+                    self.snakePoss[0][0]+=decDist
+                    length = self.snakePoss[0][2]-self.snakePoss[0][0]
+                    self.__DeleteTail(length)
+                    return
+                elif self.snakePoss[0][2]-self.snakePoss[0][0]==decDist:
+                    self.snakePoss.pop(0)
+                    return
+                else:
+                    self.snakePoss.pop(0)
+                    decDist-=(self.snakePoss[0][2]-self.snakePoss[0][0])
+            elif self._ToUp(0):
+                if self.snakePoss[0][1]-self.snakePoss[0][3]>decDist:
+                    self.snakePoss[0][1]-=decDist
+                    length = self.snakePoss[0][1]-self.snakePoss[0][3]
+                    self.__DeleteTail(length)
+                    return
+                elif self.snakePoss[0][1]-self.snakePoss[0][3]==decDist:
+                    self.snakePoss.pop(0)
+                    return
+                else:
+                    self.snakePoss.pop(0)
+                    decDist-=(self.snakePoss[0][1]-self.snakePoss[0][3])
+            elif self._ToDown(0):
+                if self.snakePoss[0][3]-self.snakePoss[0][1]>decDist:
+                    self.snakePoss[0][1]+=decDist
+                    length = self.snakePoss[0][3]-self.snakePoss[0][1]
+                    self.__DeleteTail(length)
+                    return
+                elif self.snakePoss[0][3]-self.snakePoss[0][1]==decDist:
+                    self.snakePoss.pop(0)
+                    return
+                else:
+                    self.snakePoss.pop(0)
+                    decDist-=(self.snakePoss[0][3]-self.snakePoss[0][1])
+            else:
+                print "error in _UpdateRects"
+            
+##            for direct0,direct1,diff0,diff1 in [(0,2,1,3),(1,3,0,2)]:
+##                if self.snakePoss[0][direct0]==self.snakePoss[0][direct1]:
+##                    if self.snakePoss[0][diff0]>self.snakePoss[0][diff1]:
+##                        if self.snakePoss[0][diff0]-self.snakePoss[0][diff1]>=moveDist:
+##                            self.snakePoss[0][diff0]-=moveDist
+##                            return
+##                        elif self.snakePoss[0][diff0]-self.snakePoss[0][diff1]==moveDist:
+##                            self.snakePoss.pop(0)
+##                            return
+##                        else:
+##                            moveDist-=(self.snakePoss[0][diff0]-self.snakePoss[0][diff1])
+##                            self.snakePoss.pop(0)
+##                    else:
+##                        if self.snakePoss[0][diff1]-self.snakePoss[0][diff0]>=moveDist:
+##                            self.snakePoss[0][diff0]+=moveDist
+##                            return
+##                        elif self.snakePoss[0][diff1]-self.snakePoss[0][diff0]==moveDist:
+##                            self.snakePoss.pop(0)
+##                            return
+##                        else:
+##                            moveDist-=(self.snakePoss[0][diff1]-self.snakePoss[0][diff0])
+##                            self.snakePoss.pop(0)
             
                             
     #返回值True表示撞死了
     def _SankeGo(self,pass_time):
-        moveDist = pass_time*self.v
+        moveDist = pass_time*self.v/10.0
         if self.dirChange:
             if self.direction==0:
                 if self._ToLeft(-1):
-                    self.snakePoss.append(self.snakePoss[-1][2]+SNAKE_WITH_HALH,
+                    self.snakePoss.append([self.snakePoss[-1][2]+SNAKE_WITH_HALH,
                                           self.snakePoss[-1][3]-SNAKE_WITH_HALH,
                                           self.snakePoss[-1][2]+SNAKE_WITH_HALH,
-                                          self.snakePoss[-1][3]-SNAKE_WITH_HALH-moveDist)
+                                          self.snakePoss[-1][3]-SNAKE_WITH_HALH-moveDist])
                 else :
-                    self.snakePoss.append(self.snakePoss[-1][2]-SNAKE_WITH_HALH,
+                    self.snakePoss.append([self.snakePoss[-1][2]-SNAKE_WITH_HALH,
                                           self.snakePoss[-1][3]-SNAKE_WITH_HALH,
                                           self.snakePoss[-1][2]-SNAKE_WITH_HALH,
-                                          self.snakePoss[-1][3]-SNAKE_WITH_HALH-moveDist)
+                                          self.snakePoss[-1][3]-SNAKE_WITH_HALH-moveDist])
             elif self.direction==1:
                 if self._ToLeft(-1):
-                    self.snakePoss.append(self.snakePoss[-1][2]+SNAKE_WITH_HALH,
+                    self.snakePoss.append([self.snakePoss[-1][2]+SNAKE_WITH_HALH,
                                           self.snakePoss[-1][3]+SNAKE_WITH_HALH,
                                           self.snakePoss[-1][2]+SNAKE_WITH_HALH,
-                                          self.snakePoss[-1][3]+SNAKE_WITH_HALH+moveDist)
+                                          self.snakePoss[-1][3]+SNAKE_WITH_HALH+moveDist])
                 else :
-                    self.snakePoss.append(self.snakePoss[-1][2]-SNAKE_WITH_HALH,
+                    self.snakePoss.append([self.snakePoss[-1][2]-SNAKE_WITH_HALH,
                                           self.snakePoss[-1][3]+SNAKE_WITH_HALH,
                                           self.snakePoss[-1][2]-SNAKE_WITH_HALH,
-                                          self.snakePoss[-1][3]+SNAKE_WITH_HALH+moveDist)
+                                          self.snakePoss[-1][3]+SNAKE_WITH_HALH+moveDist])
             elif self.direction==2:
                 if self._ToUp(-1):
-                    self.snakePoss.append(self.snakePoss[-1][2]-SNAKE_WITH_HALH,
+                    self.snakePoss.append([self.snakePoss[-1][2]-SNAKE_WITH_HALH,
                                           self.snakePoss[-1][3]+SNAKE_WITH_HALH,
                                           self.snakePoss[-1][2]-SNAKE_WITH_HALH-moveDist,
-                                          self.snakePoss[-1][3]+SNAKE_WITH_HALH)
+                                          self.snakePoss[-1][3]+SNAKE_WITH_HALH])
                 else :
-                    self.snakePoss.append(self.snakePoss[-1][2]-SNAKE_WITH_HALH,
+                    self.snakePoss.append([self.snakePoss[-1][2]-SNAKE_WITH_HALH,
                                           self.snakePoss[-1][3]-SNAKE_WITH_HALH,
                                           self.snakePoss[-1][2]-SNAKE_WITH_HALH-moveDist,
-                                          self.snakePoss[-1][3]-SNAKE_WITH_HALH)
+                                          self.snakePoss[-1][3]-SNAKE_WITH_HALH])
             else:
                 if self._ToUp(-1):
-                    self.snakePoss.append(self.snakePoss[-1][2]+SNAKE_WITH_HALH,
+                    self.snakePoss.append([self.snakePoss[-1][2]+SNAKE_WITH_HALH,
                                           self.snakePoss[-1][3]+SNAKE_WITH_HALH,
                                           self.snakePoss[-1][2]+SNAKE_WITH_HALH+moveDist,
-                                          self.snakePoss[-1][3]+SNAKE_WITH_HALH)
+                                          self.snakePoss[-1][3]+SNAKE_WITH_HALH])
                 else :
-                    self.snakePoss.append(self.snakePoss[-1][2]+SNAKE_WITH_HALH,
+                    self.snakePoss.append([self.snakePoss[-1][2]+SNAKE_WITH_HALH,
                                           self.snakePoss[-1][3]-SNAKE_WITH_HALH,
                                           self.snakePoss[-1][2]+SNAKE_WITH_HALH+moveDist,
-                                          self.snakePoss[-1][3]-SNAKE_WITH_HALH)
+                                          self.snakePoss[-1][3]-SNAKE_WITH_HALH])
         else:
             if self.direction==0:
                 self.snakePoss[-1][3]-=moveDist
--- game.py
+++ game.py
@@ -1,9 +1,5 @@
-import pygame
-from sys import exit
-from pygame.locals import *
-
-
-
+from Const import *
+from Snake import *
 
 
 
@@ -12,20 +8,42 @@ from pygame.locals import *
 
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 500), 0, 32) 
+screen = pygame.display.set_mode((SCREEN_WITH, SCREEN_HEIGHT), 0, 32) 
 pygame.display.set_caption("Snake!")
 
-background =  pygame.Surface((1000,500),flags=SRCALPHA, depth=32)
 
-background.fill((100,100,100))
 
+
+
+clock = pygame.time.Clock()
+t0 = clock.tick()
+
+
+snake_zhs = Snake(pygame,screen)
+
+direction =None
+applePos=None
 
 while True:
-    screen.fill((0,255,100))
-##    screen.blit(background,(0,0))
+    screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        elif event.type == KEYDOWN:
+            key = event.key
+            if key == K_UP :
+                direction=0
+            elif key == K_DOWN:
+                direction=1
+            elif key == K_LEFT:
+                direction=2
+            elif key == K_RIGHT :
+                direction=3
+            else:
+                direction =None
+            
+    time_passed = clock.tick(30)
+    snake_zhs.ForJade(time_passed,applePos,direction)
     pygame.display.update()
     
