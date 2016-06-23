bf719a8b479bfdcc874becfb9152fc5fe34bcf0d
Yajing Chen <jadechancyj907@gmail.com>
2016-06-23 19:51:28 +0800

    add game over

--- game.py
+++ game.py
@@ -1,12 +1,24 @@
 from Const import *
 from Snake import *
 from Apple import *
+import os
 
+# show game over animation using sprite technique
+def GameOver(scrn):
+    try:
+        # load image and scale to fit screen size
+        spritesheet = pygame.image.load(os.path.join('graph', 'gameover.png')).convert()
+        spritesheet = pygame.transform.scale(spritesheet, (SCREEN_WITH * 24, SCREEN_HEIGHT))
+    except:
+        raise(UserWarning, "Unable to load sprite image file")
 
+    FPS = 100
+    for i in range(24):
+        subImage = spritesheet.subsurface(SCREEN_WITH*i,0,SCREEN_WITH,SCREEN_HEIGHT)
+        scrn.blit(subImage, (0,0))
+        pygame.time.delay(FPS)
+        pygame.display.update()
 
-def GameOver(game, scrn):
-    #gameOverPic = game.image.load('game-over.gif').convert_alpha()
-    #scrn.blit(gameOverPic, (0,0))
 
 
 
@@ -14,7 +26,7 @@ def GameOver(game, scrn):
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WITH, SCREEN_HEIGHT), 0, 32) 
 pygame.display.set_caption("Snake!")
-applePic = pygame.image.load('apple.png').convert()
+applePic = pygame.image.load(os.path.join('graph', 'apple.png')).convert()
 
 
 snake_zhs = Snake(pygame,screen)
@@ -51,8 +63,10 @@ while True:
     d_t=time.time()-t_start
     time_passed = clock.tick(30)
     (snake_dead,apple_eaten)=snake_zhs.ForJade(time_passed,apple_public.SetApple(apple_eaten),direction)
-    #if snake_dead:
-    #    GameOver(pygame, screen)
+
+    if snake_dead:
+        GameOver(screen)
+        break
 
     if apple_eaten:
         score+=10
