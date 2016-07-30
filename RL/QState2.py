60f798fb7505a81ec12bb92c0a60bc78eb379c94
Yajing Chen <jadechancyj907@gmail.com>
2016-07-30 14:32:53 +0800

    modify q state and the results still suffer. will be back in a few days

--- RL/QState2.py
+++ RL/QState2.py
@@ -6,7 +6,7 @@ from random import randint
 class QFunc:
     def __init__(self):
         self.QMat = defaultdict(float)
-        self.learning_rate = 0.5
+        self.learning_rate = 0.4
         self.discount = 0.8
 
     def __is_wall__(self, x, y):
@@ -44,10 +44,10 @@ class QFunc:
         ii = 0
         for x in [head_x-1,head_x,head_x+1]:
             for y in [head_y-1,head_y,head_y+1]:
-                if self.__is_apple__(x, y, apple_pos):
-                    qstate[ii] = 3
-                elif self.__is_body__(x, y, snake_body):
+                if self.__is_body__(x, y, snake_body):
                     qstate[ii] = 1
+                elif self.__is_apple__(x, y, apple_pos):
+                    qstate[ii] = 3
                 elif self.__is_wall__(x, y):
                     qstate[ii] = 2
                 ii += 1
@@ -58,34 +58,30 @@ class QFunc:
         s_qstate = self.qstate2str(qstate)
         if state.snake.IsDead():
             for a in [Direction.North, Direction.South, Direction.West, Direction.East]:
-                self.QMat[(s_qstate, a)] = -Grid_Y * Grid_X
+                self.QMat[(s_qstate, a)] = -500
             return None
 
+        reward = -10
         if state.IsAppleEaten():
-            for a in [Direction.North, Direction.South, Direction.West, Direction.East]:
-                self.QMat[(s_qstate, a)] = Grid_Y * Grid_X
             state.GenNewApple()
             newState = state.GetNextState(d)
-            return newState
+            reward = 500
+        else:
+            newState = state.GetNextState(d)
 
-        newState = state.GetNextState(d)
         newqState = self.ExtractQState(newState)
         s_newqState = self.qstate2str(newqState)
 
         best_q = float("-inf")
-        best_a = None
         for a in [Direction.East, Direction.North, Direction.South, Direction.West]:
             q = self.QMat.get((s_newqState, a), 0)
             if q > best_q:
                 best_q = q
-                best_a = a
 
-        if best_a == d:
-            reward = 0
-        else:
-            reward = -5
         self.QMat[(s_qstate, d)] = self.QMat.get((s_qstate,d),0) + \
                                  self.learning_rate * (reward+self.discount*best_q-self.QMat.get((s_qstate,d),0))
+        if self.QMat[(s_qstate, d)] > 500:
+            self.QMat[(s_qstate, d)] = 500
         return newState
 
     def GetBestDir(self, state):
